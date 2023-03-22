8252ce82296e05278c088e1c16e738e57887cd6e
mirkodeglinnocenti <deglinnocentimirko@gmail.com>
2023-03-22 16:06:29 +0100

    FEAT print data from server added

--- index.php
+++ index.php
@@ -8,12 +8,26 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.27.2/axios.min.js" integrity="sha512-odNmoc1XJy5x1TMVMdC7EMs3IVdItLPlCeL5vSUPN2llYKMJ2eByTTAIiiuqLg+GdNr9hF6z81p27DArRFKT7A==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+    <link rel="stylesheet" href="./style.css">
 </head>
 <body>
 
     <div id="app">
-        <div class="container">
-            ciao 
+        <main>
+
+            <section class="main-section p-5 text-center">
+                <div class="container-custom">
+
+                    <h1 class="title">To do List</h1>
+
+                    <ul class="list-group">
+                        <li v-for="toDo in this.toDoList" class="list-group-item text-start text-uppercase">{{toDo}}</li>
+                    </ul>
+
+                </div>
+            </section>
+
+        </main>
     </div>
     
     <script src="./app.js"></script>
